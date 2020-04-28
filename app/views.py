e9bbca549703a531073b881cd209963631e68d33
owinolawrence <owinolawrence21@gmail.com>
2020-04-28 10:46:39 +0300

    commit news templates

--- app/views.py
+++ app/views.py
@@ -2,10 +2,12 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
+@app.route('/news/<news_id>')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)

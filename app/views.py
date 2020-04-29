90179ecdb6a99d6f5755cb290b9fb61de688ff34
owinolawrence <owinolawrence21@gmail.com>
2020-04-29 10:51:42 +0300

    commit new virtual

--- app/views.py
+++ app/views.py
@@ -2,14 +2,14 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route()
+@app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
     # Getting popular news
-    popular_news = get_news('popular')
+    # popular_news = get_news('popular')
     upcoming_news = get_news('upcoming')
     now_showing_news = get_news('now_showing')
     title = 'Home - Welcome to The best News Review Website Online'
